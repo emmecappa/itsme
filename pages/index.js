192211b39281be22be0cb1030ca9ad9d9400769b
emmecappa <117775229+emmecappa@users.noreply.github.com>
2023-02-27 15:09:06 +0000

    feature: add noise everywhere

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import style from "../styles/home.module.css"
+
 
 function Home() {
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
+    <main className="flex flex-col items-center justify-center h-screen noise-background">
       <h1 className="text-6xl font-bold text-amber-400">Hello it's me :)</h1>
       <h2 className="font-bold text-amber-600">mammata</h2>
     </main>
