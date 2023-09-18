cf8f3ac44cc78ac77a7e57be82cbc7070015cdb9
emmecappa <117775229+emmecappa@users.noreply.github.com>
2023-09-18 12:55:16 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center h-screen noise-background">
-      <h1 className="text-6xl font-bold text-amber-400">Hello it's me :)</h1>
-      <h2 className="font-bold text-amber-600">mammata</h2>
+     	<h1 className="text-6xl font-bold text-amber-400">Hello it's me :)</h1>
+    	<h2 className="font-bold text-amber-600">mammata</h2>
+    	<p> ti do fastidio? pazienza. </p>
     </main>
   )
 }
