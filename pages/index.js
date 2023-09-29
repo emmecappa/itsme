4ea31753b386483233937230551126543011dc70
emmecappa <117775229+emmecappa@users.noreply.github.com>
2023-09-29 07:59:06 +0200

    feature: new homepage text

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,8 @@ import style from "../styles/home.module.css"
 function Home() {
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen noise-background">
-     	<h1 className="text-6xl font-bold text-amber-400">Hello it's me :)</h1>
-    	<h2 className="font-bold text-amber-600">mammata</h2>
-    	<p> ti do fastidio? pazienza. </p>
+    <main>
+      Hello my name is Marco Casto, but everybody calls me MK.
     </main>
   )
 }
