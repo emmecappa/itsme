5df4dabe135450e3eaec08807a8cfb087b08cc74
emmecappa <117775229+emmecappa@users.noreply.github.com>
2023-09-29 08:47:11 +0200

    feature: add tailwind to homepage

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,20 @@ import style from "../styles/home.module.css"
 function Home() {
 
   return (
-    <div class="container">
-      <header class="header">
-          <h1>Hello my name is Marco Casto, but everybody calls me MK.</h1>
+    <div class="flex flex-col min-h-screen">
+      <header class="py-4">
+          <div class="container mx-auto">
+              <h1 class="text-2xl font-bold">Hello my name is Marco Casto, but everybody calls me MK.</h1>
+          </div>
       </header>
-      <main class="content">
-          
+
+      <main class="flex-grow container mx-auto py-8">
+          <h2 class="text-2xl">Coming soon</h2>
       </main>
-      <footer class="footer">
-          
+
+      <footer class="py-4">
+          <div class="container mx-auto">
+          </div>
       </footer>
     </div>
   )
