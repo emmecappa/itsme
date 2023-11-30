778060a4b343f8a1be35a0f18d5a1fbb8c31754c
emmecappa <117775229+emmecappa@users.noreply.github.com>
2023-11-30 16:49:40 +0000

    just for fun

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,15 @@ import style from "../styles/home.module.css"
 function Home() {
 
   return (
-    <div class="flex items-center justify-center h-screen">
+    <div class="flex flex-col items-center justify-center h-screen">
       <header class="py-4">
           <div class="container mx-auto">
-              <h1 class="text-2xl font-bold">Hello my name is Marco Casto, but everybody calls me MK.</h1>
+              <h1 class="text-2xl font-bold text-center">Hello my name is Marco Casto, but everybody calls me MK.</h1>
           </div>
       </header>
 
       <main class="flex-grow container mx-auto py-8">
-          <h2 class="text-2xl">Coming soon</h2>
+          <h2 class="text-2xl text-center">Coming soon</h2>
       </main>
 
       <footer class="py-4">
